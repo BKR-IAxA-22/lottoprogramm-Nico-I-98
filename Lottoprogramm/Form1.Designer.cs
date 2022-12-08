6bd4f502eb06761f98a4524bb4603b000ba31247
Nico Imholz <1058412@bkrnet.de>
2022-12-08 08:26:21 +0100

    Oberfläche angepasst

--- Lottoprogramm/Form1.Designer.cs
+++ Lottoprogramm/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.Zahl1 = new System.Windows.Forms.Label();
             this.Zahl2 = new System.Windows.Forms.Label();
@@ -35,79 +36,167 @@
             this.Zahl4 = new System.Windows.Forms.Label();
             this.Zahl5 = new System.Windows.Forms.Label();
             this.Zahl6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.button1.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.button1.Location = new System.Drawing.Point(187, 185);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(384, 69);
+            this.button1.Size = new System.Drawing.Size(394, 100);
             this.button1.TabIndex = 0;
             this.button1.Text = "Generate";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Zahl1
             // 
-            this.Zahl1.AutoSize = true;
-            this.Zahl1.Location = new System.Drawing.Point(184, 74);
+            this.Zahl1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Zahl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Zahl1.Location = new System.Drawing.Point(187, 73);
             this.Zahl1.Name = "Zahl1";
-            this.Zahl1.Size = new System.Drawing.Size(34, 13);
+            this.Zahl1.Size = new System.Drawing.Size(40, 31);
             this.Zahl1.TabIndex = 1;
             this.Zahl1.Text = "Zahl1";
+            this.Zahl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Zahl2
             // 
-            this.Zahl2.AutoSize = true;
-            this.Zahl2.Location = new System.Drawing.Point(253, 74);
+            this.Zahl2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Zahl2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Zahl2.Location = new System.Drawing.Point(255, 72);
             this.Zahl2.Name = "Zahl2";
-            this.Zahl2.Size = new System.Drawing.Size(34, 13);
+            this.Zahl2.Size = new System.Drawing.Size(40, 31);
             this.Zahl2.TabIndex = 2;
             this.Zahl2.Text = "Zahl2";
+            this.Zahl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Zahl3
             // 
-            this.Zahl3.AutoSize = true;
-            this.Zahl3.Location = new System.Drawing.Point(330, 74);
+            this.Zahl3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Zahl3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Zahl3.Location = new System.Drawing.Point(326, 72);
             this.Zahl3.Name = "Zahl3";
-            this.Zahl3.Size = new System.Drawing.Size(34, 13);
+            this.Zahl3.Size = new System.Drawing.Size(40, 32);
             this.Zahl3.TabIndex = 3;
             this.Zahl3.Text = "Zahl3";
+            this.Zahl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Zahl4
             // 
-            this.Zahl4.AutoSize = true;
-            this.Zahl4.Location = new System.Drawing.Point(401, 74);
+            this.Zahl4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Zahl4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Zahl4.Location = new System.Drawing.Point(397, 73);
             this.Zahl4.Name = "Zahl4";
-            this.Zahl4.Size = new System.Drawing.Size(34, 13);
+            this.Zahl4.Size = new System.Drawing.Size(40, 31);
             this.Zahl4.TabIndex = 4;
             this.Zahl4.Text = "Zahl4";
+            this.Zahl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Zahl5
             // 
-            this.Zahl5.AutoSize = true;
-            this.Zahl5.Location = new System.Drawing.Point(475, 74);
+            this.Zahl5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Zahl5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Zahl5.Location = new System.Drawing.Point(467, 73);
             this.Zahl5.Name = "Zahl5";
-            this.Zahl5.Size = new System.Drawing.Size(34, 13);
+            this.Zahl5.Size = new System.Drawing.Size(40, 32);
             this.Zahl5.TabIndex = 5;
             this.Zahl5.Text = "Zahl5";
+            this.Zahl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Zahl6
             // 
-            this.Zahl6.AutoSize = true;
-            this.Zahl6.Location = new System.Drawing.Point(547, 74);
+            this.Zahl6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Zahl6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Zahl6.Location = new System.Drawing.Point(541, 73);
             this.Zahl6.Name = "Zahl6";
-            this.Zahl6.Size = new System.Drawing.Size(34, 13);
+            this.Zahl6.Size = new System.Drawing.Size(40, 31);
             this.Zahl6.TabIndex = 6;
             this.Zahl6.Text = "Zahl6";
+            this.Zahl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(187, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 32);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(255, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 32);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(326, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(40, 32);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(397, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(40, 32);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(467, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(40, 32);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(541, 129);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(40, 32);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(750, 323);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Zahl6);
             this.Controls.Add(this.Zahl5);
             this.Controls.Add(this.Zahl4);
@@ -131,6 +220,12 @@
         private System.Windows.Forms.Label Zahl4;
         private System.Windows.Forms.Label Zahl5;
         private System.Windows.Forms.Label Zahl6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
